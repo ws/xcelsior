eb2f2a1f2c6b5e47107155d44179b6f16cd9b851
Will Smidlein <will@willsmidlein.com>
2023-08-18 15:36:41 -0600

    Add some more endpoints

--- src/lib/meter.ts
+++ src/lib/meter.ts
@@ -15,3 +15,50 @@ export const getDeviceInformation = async (
 
   return obj;
 };
+
+interface DeviceTime {
+  currentTime: number;
+}
+
+export const getDeviceTime = async (
+  session: Readonly<AxiosInstance>,
+): Promise<DeviceTime> => {
+  const response = await session.get('/tm');
+  const xml = response.data;
+  const { Time: obj } = fromXML(xml);
+  const currentTime = parseInt(obj.currentTime);
+
+  return { currentTime };
+};
+
+interface MeterReading {
+  value: number;
+  timePeriod: {
+    duration: number;
+    start: number;
+  };
+}
+
+const parseReading = (xml: string) => {
+  const { Reading: obj } = fromXML(xml);
+
+  const value = parseInt(obj.value);
+  const timePeriod = {
+    duration: parseInt(obj.timePeriod.duration),
+    start: parseInt(obj.timePeriod.start),
+  };
+
+  return {
+    value,
+    timePeriod,
+  };
+};
+
+export const getInstantaneousDemand = async (
+  session: Readonly<AxiosInstance>,
+): Promise<MeterReading> => {
+  const response = await session.get('/upt/1/mr/1/r');
+  const xml = response.data;
+
+  return parseReading(xml);
+};
