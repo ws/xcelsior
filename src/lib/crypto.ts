3735e019d30c3b10c09d2f227e4e6e7e2c847f32
Will Smidlein <will@willsmidlein.com>
2023-08-20 13:01:27 -0600

    Add cert generation in pure JS

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -26,6 +26,18 @@ export const isValidCredentials = (
   }
 };
 
+const generateRandomBigInt = (bits: number): bigint => {
+  const bytes = new Uint8Array(Math.ceil(bits / 8));
+  crypto.getRandomValues(bytes);
+  return (
+    Array.from(bytes).reduce(
+      (acc, byte) => (acc << BigInt(8)) | BigInt(byte),
+      BigInt(0),
+    ) &
+    ((BigInt(1) << BigInt(bits)) - BigInt(1))
+  );
+};
+
 export const getLfdi = async (cert: CertPem): Promise<string> => {
   const certificate = new x509.X509Certificate(cert);
 
@@ -41,4 +53,56 @@ export const getLfdi = async (cert: CertPem): Promise<string> => {
       .toUpperCase() || '';
 
   return lfdi;
+};
+
+export const generateCertificate = async (appName = 'xcelsior') => {
+  const alg = {
+    name: 'ECDSA',
+    namedCurve: 'P-256',
+    hash: 'SHA-256',
+  };
+
+  // Generate keys (making sure the private key is extractable)
+  const keys = await crypto.subtle.generateKey(alg, true, ['sign', 'verify']);
+
+  const randomHex = [...Array(6)]
+    .map(() =>
+      Math.floor(Math.random() * 0x1000000)
+        .toString(16)
+        .padStart(6, '0'),
+    )
+    .join('');
+
+  // build name as instructed by example app
+  // https://github.com/Xcel-Energy/energy-launchpadsdk-client/blob/f44c6ee63763af0b6d9a699ab4a9d80de8592196/src/Ieee2030dot5/Ieee20305dot5Authentication/CertificateUtility.cs#L53
+  const name = `O=${appName},CN=${randomHex}`;
+
+  // Create self-signed certificate
+  const certificate = await x509.X509CertificateGenerator.createSelfSigned({
+    serialNumber: `${generateRandomBigInt(64)}`,
+    name,
+    notBefore: new Date(),
+    notAfter: new Date(Date.now() + 3 * 365 * 24 * 60 * 60 * 1000), // expire in 3 years (max allowed)
+    signingAlgorithm: alg,
+    keys,
+    extensions: [
+      new x509.BasicConstraintsExtension(false, 0, true),
+      new x509.KeyUsagesExtension(x509.KeyUsageFlags.digitalSignature, true), // https://github.com/Xcel-Energy/energy-launchpadsdk-client/blob/f44c6ee63763af0b6d9a699ab4a9d80de8592196/src/Ieee2030dot5/Ieee20305dot5Authentication/CertificateUtility.cs#L92
+      new x509.CertificatePolicyExtension(['1.3.6.1.4.1.40732.2.2'], true), // https://github.com/Xcel-Energy/energy-launchpadsdk-client/blob/f44c6ee63763af0b6d9a699ab4a9d80de8592196/src/Ieee2030dot5/Ieee20305dot5Authentication/CertificateUtility.cs#L98
+    ],
+  });
+
+  // Export the private key as PKCS#8
+  const exportedPrivateKey = await crypto.subtle.exportKey(
+    'pkcs8',
+    keys.privateKey,
+  );
+
+  // Convert to PEM format
+  const base64 = Buffer.from(exportedPrivateKey).toString('base64');
+  const key = `-----BEGIN PRIVATE KEY-----\n${base64}\n-----END PRIVATE KEY-----`;
+
+  const cert = certificate.toString('pem');
+
+  return { cert, key };
 };
--- src/tests/crypto.spec.ts
+++ src/tests/crypto.spec.ts
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import { getLfdi } from '../lib/crypto';
+import { generateCertificate, getLfdi } from '../lib/crypto';
 
 import getFakeKeyAndCert from './utils/getFakeKeyAndCert';
 
@@ -9,4 +9,17 @@ test('getLfdi with valid cert', async (t) => {
 
   const result = await getLfdi(cert);
   t.is(result, '4427E-F88A6-068F0-6D225-D64CC-4E23C-4D734-E848D');
+});
+
+test.only('generate a cert', async (t) => {
+  const { cert, key } = await generateCertificate();
+
+  t.is(typeof cert, 'string');
+  t.true(cert.startsWith('-----BEGIN CERTIFICATE-----'));
+  t.true(cert.endsWith('-----END CERTIFICATE-----'));
+  t.is(typeof key, 'string');
+  t.true(key.startsWith('-----BEGIN PRIVATE KEY-----'));
+  t.true(key.endsWith('-----END PRIVATE KEY-----'));
+
+  // todo: actually validate the cert
 });
